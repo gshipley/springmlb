9b9294d7f3633830f909eb8303976842c1bcea27
Grant Shipley <author@grantshipley.com>
2014-03-26 15:52:04 -0600

    code fix

--- src/main/java/org/springapp/config/WebMvcConfig.java
+++ src/main/java/org/springapp/config/WebMvcConfig.java
@@ -1,6 +1,6 @@
 package org.springapp.config;
 
-import org.springapp.rest.PingResource;
+import org.springapp.rest.MLBParkResource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
